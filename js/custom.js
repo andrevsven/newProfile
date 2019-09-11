a395b8c4d06b3890e5e95e59fb2d9a72538aa9e9
andrevsven <andrevsven@gmail.com>
2019-09-11 12:36:13 +0300

    slide

--- js/custom.js
+++ js/custom.js
@@ -1,18 +1,11 @@
-$(function(){
-
-	var hf = function(){
-		var h_header = $('header').height();
-		var h_footer = $('footer').height();
-		$('.content').css({
-			'paddingTop': h_header,
-			'paddingBottom': h_footer
-		});
-	}
-
-	$(window).bind('load resize', hf);
-
+new fullpage('#fullpage', {
+  
 });
 
+//adding the action to the button
+$(document).on('click', '#moveUp', function(){
+  fullpage_api.moveSectionUp();
+});
    
 
 
